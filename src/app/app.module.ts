4e598ecd5f345b6cff4f3059fb87a89231f61654
muajadev <ericks28@gmail.com>
2020-09-14 15:49:53 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { RolModel } from './componentes/modelo/rol.model';
+import { RolService } from './componentes/servicios/rol.service';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, RolModel ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, RolService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
